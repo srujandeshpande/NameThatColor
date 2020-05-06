a233c4c51f428ed31d85e3a8347dd269bb93ba27
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-06 14:48:47 +0530

    json partially working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,90 @@
+use std::collections::HashMap;
+use std::io;
+
+fn mrp() -> Result<(), Box<dyn std::error::Error>> {
+
+    println!("Enter the Hex");
+    let mut hex = String::new();
+    io::stdin()
+        .read_line(&mut hex)
+        .expect("Failed to read line");
+
+    println!("Your color is {}", hex);
+
+    let request_url = format!("https://api.color.pizza/v1/{hex}", hex=hex);
+    println!("{}", request_url);
+
+    let rr = "https://httpbin.org/ip";
+    let resp = reqwest::blocking::get(&request_url)?
+        .json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
+}
+
+fn main() {
+    mrp();
+}
+
+/*
+
+extern crate reqwest;
+    extern crate serde;
+
+     use reqwest::Error;
+     use serde::Deserialize;
+
+    // This `derive` requires the `serde` dependency.
+     #[derive(Deserialize)]
+     struct Ip {
+         origin: String,
+     }
+/*
+     async fn main() -> Result<(), Error> {
+         println!("Here3");
+
+     let ip = reqwest::get("http://httpbin.org/ip")
+         .await?
+         .json::<Ip>()
+         .await?;
+
+     println!("ip: {}", ip.origin);
+      Ok(())
+      }
+*/
+
+fn main(){
+    #[derive(Deserialize)]
+
+struct Ip {
+    origin: String,
+}
+
+let json: Ip = reqwest::blocking::get("http://httpbin.org/ip").json();
+//reqwest::get("http://httpbin.org/ip")?.json()?;
+}
+*/
+/*
+    #[derive(Deserialize)]
+struct Ip {
+    origin: String,
+}
+
+let ip = reqwest::get("http://httpbin.org/ip").json::<Ip>();
+
+async fn run() -> Result<(), Box<dyn std::error::Error>> {
+    let bytes = reqwest::get("http://httpbin.org/ip")
+         .await?
+         .bytes()
+         .await?;
+
+     println!("bytes: {:?}", bytes);
+     Ok(())
+     }
+
+}
+*/
+
+/*
 extern crate reqwest;
 
 use std::io;
@@ -27,3 +114,4 @@ fn main() {
     }
 
 }
+*/
