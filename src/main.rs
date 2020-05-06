7fc25cc004aa0a90859d0ed283e33094395fdd10
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-06 18:25:30 +0530

    not working json

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
+extern crate serde;
+extern crate serde_json;
+
+//#[macro_use] extern crate serde_derive;
+extern crate serde_derive;
 use serde::{Deserialize};
 //use std::collections::HashMap;
 use std::io;
 
+
+
+
+
 fn mrp() -> Result<(String), Box<dyn std::error::Error>> {
-    #[derive(Deserialize)]
+    //#[derive(Serialize, Deserialize, Debug)]
+    #[derive(Deserialize, Debug)]
+
     struct Ip {
-        origin: String,
+        colors: Vec<String>,
+    }
+    #[derive(Deserialize, Debug)]
+    struct Color {
+        hex: String,
+        name: String,
+        rgb: rgb,
+        requestedHex: String,
+        luminance: f32,
+        distance: f32,
+    }
+    #[derive(Deserialize, Debug)]
+    struct rgb {
+        r: u32,
+        g: u32,
+        b: u32,
     }
 
     println!("Enter the Hex");
@@ -16,24 +42,35 @@ fn mrp() -> Result<(String), Box<dyn std::error::Error>> {
 
     println!("Your color is {}", hex);
 
-    //let request_url = format!("https://api.color.pizza/v1/{hex}", hex=hex);
-    let request_url = "https://httpbin.org/ip";
+    let request_url = format!("https://api.color.pizza/v1/{hex}", hex=hex);
+    //let request_url = "https://httpbin.org/ip";
     println!("{}", request_url);
 
     //let resp = reqwest::blocking::get(request_url)?
     //    .json::<HashMap<String, String>>()?;
 
-    let resp = reqwest::blocking::get(request_url)?
+    let resp = reqwest::blocking::get(&request_url)?
         .json::<Ip>()?;
+
+    //let resp = reqwest::blocking::get(&request_url)?
+    //    .text()?;
 
     //let json: Ip = reqwest::blocking::get("http://httpbin.org/ip")
     //    .json();
     //let ip = reqwest::blocking::get("http://httpbin.org/ip")?
     //    .json::<Ip>()?;
+    //for i in resp.color
+    //let col = resp.colors[0].json::<Color>()?;
+    if resp.colors[0][0] == 239 {
+    resp.colors[0].remove(0);
+}
+    let deserialized: Value = serde_json::from_str(&resp.colors[0]).unwrap();
+    println!("deserialized = {:#?}", deserialized.name);
+    //println!("{}", resp.colors);
 
-    println!("{}", resp.origin);
     //Ok(())
-    return Ok (resp.origin);
+    return Ok(request_url);
+    //return Ok (resp.colors[0]);
 }
 
 fn main() {
