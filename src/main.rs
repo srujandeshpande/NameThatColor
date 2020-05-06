7e1974ec6993c63060f00a6086252820dc4841ae
Srujan Deshpande <srujan.deshpande@gmail.com>
2020-05-06 11:51:19 +0530

    line input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Enter the Hex");
+
+    let mut hex = String::new();
+
+    io::stdin()
+        .read_line(&mut hex)
+        .expect("Failed to read line");
+
+    println!("Your color is {}", hex);
 }
